72e5d084fc4286bff806e11d0909256fa6c01a0e
Manuel Bautista <b0hcoder@gmail.com>
2018-07-11 17:10:48 -0400

    setting login page.

---
+++ PreSys/PreSys/assets/dist/js/controllers/loginController.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module('presysLogin', []);
+
+app.controller('loginController', function ($scope, $http) {
+    $scope.username = "";
+    $scope.password = "";
+
+    //submit login
+    $scope.login = function () {
+        $(".preloader").show();
+
+        $http({
+            method: "POST",
+            url: "/Login/GetUsers",
+            data: { user: $scope.username, password: $scope.password }
+        }).then(function mySuccess(response) {
+            window.location.href = "/Home/";
+        }, function myError(response) {
+            $scope.myWelcome = response.statusText;
+        });
+    }
+});
